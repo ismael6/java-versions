6fe3cd3381bdbfa9a7f9725301338409e780bbe8
ismael6 <isma.sp6@gmail.com>
2023-09-18 15:57:58 -0600

    java8-nio:end

---
+++ src/test/java/com/isma/javaversions/v8/nio2/FunctionalWay.java
@@ -0,0 +1,37 @@
+package com.isma.javaversions.v8.nio2;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class FunctionalWay {
+
+    @Test
+    public void list() throws IOException {
+        Files.list(Paths.get(".").toRealPath())
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void walk() throws IOException {
+        Files.walk(Paths.get(".").toRealPath())
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void find() throws IOException {
+        Files.find(
+                        Paths.get("./src/").toRealPath(),
+                        6,
+                        (path, attributes) -> path.toString().endsWith(".java") && attributes.isRegularFile())
+                .forEach(System.out::println);
+    }
+
+    @Test
+    public void lines() throws IOException {
+        Files.lines(Paths.get("./src/test/resources/cinema.json"))
+                .forEach(System.out::println);
+    }
+}
