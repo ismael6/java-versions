bb707a1fa243641ec1f4e780291c218d6db34f9a
ismael6 <isma.sp6@gmail.com>
2023-12-04 11:58:55 -0600

    Java16 (#33)
    * java16
    
    * java 17

---
+++ src/test/java/com/isma/javaversions/v17/switchimprovements/GuardedPatterns.java
@@ -0,0 +1,34 @@
+package com.isma.javaversions.v17.switchimprovements;
+
+import org.junit.jupiter.api.Test;
+
+public class GuardedPatterns {
+
+    public void before(Object o){
+        switch(o) {
+            case String s:
+                if(s.length() == 1){
+                    String.format("char");
+                } else {
+                    String.format("word");
+                } break;
+            default:
+                String.format("any");
+        }
+    }
+
+    public String usingGuardedPatterns(Object o){
+        return switch(o) {
+            case String s when s.length() == 1 -> String.format("char %s", s);
+            case String s -> String.format("any String %s", s);
+            default -> String.format("any ", o.toString());
+        };
+    }
+
+    @Test
+    public void test(){
+        System.out.println(usingGuardedPatterns("r"));
+        System.out.println(usingGuardedPatterns("word"));
+        System.out.println(usingGuardedPatterns(5));
+    }
+}
---
+++ src/test/java/com/isma/javaversions/v17/switchimprovements/SwitchPatternMatching.java
@@ -0,0 +1,40 @@
+package com.isma.javaversions.v17.switchimprovements;
+
+import org.junit.jupiter.api.Test;
+
+public class SwitchPatternMatching {
+
+    public String before(Object o) {
+        String formatted = "?";
+        if (o instanceof Integer i) {
+            formatted = String.format("int %d", i);
+        } else if (o instanceof Long l) {
+            formatted = String.format("long %d", l);
+        } else if (o instanceof Double d) {
+            formatted = String.format("double %d", d);
+        } else if (o instanceof String s) {
+            formatted = String.format("String %d", s);
+        }
+        return formatted;
+    }
+
+    public String usingSwitchPatternMatching(Object o) {
+        return switch (o) {
+            case null -> "null";
+            case Integer i -> String.format("int %d", i);
+            case Long l -> String.format("long %d", l);
+            case Double d -> String.format("double %s", d);
+            case String s -> String.format("String %s", s);
+            default -> o.toString();
+        };
+    }
+
+    @Test
+    public void test(){
+        System.out.println(usingSwitchPatternMatching(3));
+        System.out.println(usingSwitchPatternMatching(3L));
+        System.out.println(usingSwitchPatternMatching(3D));
+        System.out.println(usingSwitchPatternMatching("3"));
+        System.out.println(usingSwitchPatternMatching(new SwitchPatternMatching()));
+    }
+}
