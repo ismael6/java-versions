7d1e3c825edc6b98ffd2175acf1bfccb4504f23b
ismael6 <isma.sp6@gmail.com>
2023-12-06 14:11:44 -0600

    java19:virtual threads

--- src/test/java/com/isma/javaversions/v19/preview/threads/VirtualThreads.java
+++ src/test/java/com/isma/javaversions/v19/preview/threads/VirtualThreads.java
@@ -1,6 +1,11 @@
 package com.isma.javaversions.v19.preview.threads;
 
 
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
+
 /**
  *
  * +++ OS Thread -> Operating System Thread
@@ -18,6 +23,7 @@ package com.isma.javaversions.v19.preview.threads;
  *
  * -------------------------java19-------------------------
  * +++ Virtual Threads -> Java thread (like inversion of control on threads)
+ *      java entity (not a SO thread wrapper)
  *      Managed by JVM
  *          IDLE state -> goes to the heap
  *          Working state -> Use some platform thread as work station
@@ -27,4 +33,32 @@ package com.isma.javaversions.v19.preview.threads;
  */
 public class VirtualThreads {
 
+    @Test
+    public void before(){
+        var counter = new AtomicInteger();
+        var i = 0;
+        while(i < 1_500_000){
+            new Thread(()-> {
+                int count = counter.incrementAndGet();
+                System.out.println("Thread: " + count);
+                LockSupport.park();
+            }).start();
+            i++;
+        }
+    }
+
+    // check in visual VM
+    @Test
+    public void virtualThreads(){
+        var counter = new AtomicInteger();
+        var i = 0;
+        while(i < 1_500_000){
+            Thread.startVirtualThread(()-> {
+                int count = counter.incrementAndGet();
+                System.out.println("Thread: " + count);
+                LockSupport.park();
+            });
+            i++;
+        }
+    }
 }
